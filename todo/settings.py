7ceb6e5ddc8b131199d3228c702e83f2cea2069b
Shamankub <shaman_kub@mail.ru>
2021-12-26 00:59:07 +0300

    Lesson 1. Usersapp, user model and superuser added. Created base API.

--- todo/settings.py
+++ todo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=bja3_zq^_-)$-p#=%e2a-!pva1tkgzxn4^_%y-9(2b9kqdili'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,13 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'usersapp',
 ]
+
+# Auth model
+AUTH_USER_MODEL = "usersapp.User"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
---
+++ usersapp/models.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from uuid import uuid4
+
+
+class User(AbstractUser):
+    uuid = models.UUIDField(primary_key=True, default=uuid4(), verbose_name="UUID")
+    username = models.CharField(unique=True, max_length=64, null=False, verbose_name="имя пользователя")
+    first_name = models.CharField(max_length=64, verbose_name="имя")
+    last_name = models.CharField(max_length=64, verbose_name="фамилия")
+    email = models.EmailField(unique=True, max_length=254, null=False, verbose_name="электронная почта")
