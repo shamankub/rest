3ebbe9574e89561645aa7f97dc2aaa65d1d97fa0
Shamankub <shaman_kub@mail.ru>
2022-01-15 14:17:51 +0300

    Lesson 3. Created TODO model

--- todoapp/models.py
+++ todoapp/models.py
@@ -9,3 +9,12 @@ class Project(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class TODO(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    text = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    creator = models.ForeignKey(User, on_delete=models.PROTECT)
+    is_active = models.BooleanField(default=True)
