bae0bd5b736ce6b06959ba149a65073d59832ee2
Shamankub <shaman_kub@mail.ru>
2022-01-27 21:53:35 +0300

    Lesson 6. All done.

--- todo/urls.py
+++ todo/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 
 from todoapp.views import ProjectViewSet, TODOViewSet
@@ -28,5 +29,6 @@ router.register("projects", ProjectViewSet)
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
+    path('api-token-auth/', views.obtain_auth_token),
     path("api/", include(router.urls)),
 ]
--- usersapp/models.py
+++ usersapp/models.py
@@ -5,8 +5,11 @@ from django.db import models
 
 
 class User(AbstractUser):
-    uuid = models.UUIDField(primary_key=True, default=uuid4(), verbose_name="UUID")
-    username = models.CharField(unique=True, max_length=64, null=False, verbose_name="имя пользователя")
+    uuid = models.UUIDField(
+        primary_key=True, default=uuid4, verbose_name="UUID")
+    username = models.CharField(
+        unique=True, max_length=64, null=False, verbose_name="имя пользователя")
     first_name = models.CharField(max_length=64, verbose_name="имя")
     last_name = models.CharField(max_length=64, verbose_name="фамилия")
-    email = models.EmailField(unique=True, max_length=254, null=False, verbose_name="электронная почта")
+    email = models.EmailField(
+        unique=True, max_length=254, null=False, verbose_name="электронная почта")
