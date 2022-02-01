bc65ceedb2899b48e9b849bdfa22129672a68942
Shamankub <shaman_kub@mail.ru>
2022-02-01 22:46:54 +0300

    Lesson 8. Code style fix

--- todoapp/tests.py
+++ todoapp/tests.py
@@ -4,9 +4,7 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from mixer.backend.django import mixer
 from rest_framework import status
-from rest_framework.test import (APIClient, APIRequestFactory,
-                                 APISimpleTestCase, APITestCase,
-                                 force_authenticate)
+from rest_framework.test import APIClient, APIRequestFactory, APISimpleTestCase, APITestCase, force_authenticate
 
 from .models import TODO, Project
 from .views import ProjectViewSet, TODOViewSet
@@ -32,17 +30,20 @@ class TestProjectViewSet(TestCase):
     def test_create_project(self):
         factory = APIRequestFactory()
         request = factory.post(
-            "/api/projects/", {"name": "New project", "repository": "http://somelink.ru", "users": []}, format="json")
+            "/api/projects/", {"name": "New project", "repository": "http://somelink.ru", "users": []}, format="json"
+        )
         view = ProjectViewSet.as_view({"post": "create"})
         response = view(request)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_create_admin(self):
         factory = APIRequestFactory()
-        admin = User.objects.create_superuser(
-            "admin", "admin@admin.com", "admin123456")
+        admin = User.objects.create_superuser("admin", "admin@admin.com", "admin123456")
         request = factory.post(
-            "/api/projects/", {"name": "New project", "repository": "http://somelink.ru", "users": [admin.uuid]}, format="json")
+            "/api/projects/",
+            {"name": "New project", "repository": "http://somelink.ru", "users": [admin.uuid]},
+            format="json",
+        )
         force_authenticate(request, admin)
         view = ProjectViewSet.as_view({"post": "create"})
         response = view(request)
@@ -55,15 +56,14 @@ class TestTODOViewSet(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_edit_admin(self):
-        admin = User.objects.create_superuser(
-            "admin", "admin@admin.com", "admin123456")
-        project = Project.objects.create(
-            name="New project", repository="http://somelink.ru")
-        todo = TODO.objects.create(
-            project=project, creator=admin, text="some text")
+        admin = User.objects.create_superuser("admin", "admin@admin.com", "admin123456")
+        project = Project.objects.create(name="New project", repository="http://somelink.ru")
+        todo = TODO.objects.create(project=project, creator=admin, text="some text")
         self.client.login(username="admin", password="admin123456")
         response = self.client.put(
-            f"/api/todos/{todo.id}/", {"project": project.id, "creator": admin.uuid, "text": "some text"},)
+            f"/api/todos/{todo.id}/",
+            {"project": project.id, "creator": admin.uuid, "text": "some text"},
+        )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         todo = TODO.objects.get(id=todo.id)
         self.assertEqual(todo.text, "some text")
@@ -71,11 +71,16 @@ class TestTODOViewSet(APITestCase):
 
     def test_edit_TODO_mixer(self):
         todo = mixer.blend(TODO)
-        admin = User.objects.create_superuser(
-            "admin", "admin@admin.com", "admin123456")
+        admin = User.objects.create_superuser("admin", "admin@admin.com", "admin123456")
         self.client.login(username="admin", password="admin123456")
-        response = self.client.put(f"/api/todos/{todo.id}/", {
-                                   "project": todo.project.id, "creator": todo.creator.uuid, "text": "some text", })
+        response = self.client.put(
+            f"/api/todos/{todo.id}/",
+            {
+                "project": todo.project.id,
+                "creator": todo.creator.uuid,
+                "text": "some text",
+            },
+        )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         todo = TODO.objects.get(id=todo.id)
         self.assertEqual(todo.text, "some text")
